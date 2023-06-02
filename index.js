a9d6ef31be6e65ec6a138aafdbd3aa8532b71de8
sandeep7788 <37241980+sandeep7788@users.noreply.github.com>
2023-06-02 19:52:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World. it is working.')
+  res.send('Hello World. it is working 1 .')
 })
 
 app.listen(port, () => {
